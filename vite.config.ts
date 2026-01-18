3c9cf9203b43f2c5490e49a512b0b40670c52517
Alp-Balaj <alpbalaj1203@gmail.com>
2026-01-18 18:24:02 +0100

    Changes made for publishing capabilities.

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 6969,
-    strictPort: true,
-    https: {
-      key: fs.readFileSync("localhost+2-key.pem"),
-      cert: fs.readFileSync("localhost+2.pem"),
+export default defineConfig(({ command }) => {
+  const isDev = command === "serve";
+
+  return {
+    plugins: [react()],
+    server: isDev
+      ? {
+          port: 6969,
+          strictPort: true,
+          https: {
+            key: fs.readFileSync("localhost+2-key.pem"),
+            cert: fs.readFileSync("localhost+2.pem"),
+          },
+        }
+      : undefined,
+    resolve: {
+      dedupe: ["react", "react-dom"],
     },
-  },
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  }
+  };
 });
