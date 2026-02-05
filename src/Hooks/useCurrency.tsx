3d3a9e17f234badbff84772fbc858783395bfbc8
Alp-Balaj <alpbalaj1203@gmail.com>
2026-02-05 20:05:35 +0100

    Issue fixed for deplyoment

--- src/Hooks/useCurrency.tsx
+++ src/Hooks/useCurrency.tsx
@@ -3,19 +3,17 @@ import { useAuthorizationApi } from "./useAuthorizationApi";
 
 type UseCurrencyReturn = {
   formatAndConvert: (amount: number, originalCurrencyId: number) => string;
-  isLoading?: boolean;           
-  error?: string | null;         
+  isLoading?: boolean;
+  error?: string | null;
 };
 
 export function useFormatedCurrency(): UseCurrencyReturn {
+  // const { currencyId } = useCurrency();
+  // const { getAllData } = useAuthorizationApi();
 
-    const { currencyId } = useCurrency();
-    const { getAllData } = useAuthorizationApi();
-
-
-    return{
-        formatAndConvert: (amt, currId) => `${amt} ${currId}`,
-        isLoading: false,
-        error: null,
-    }
+  return {
+    formatAndConvert: (amt, currId) => `${amt} ${currId}`,
+    isLoading: false,
+    error: null,
+  };
 }
